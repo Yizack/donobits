661f8de16326859a3e22ae1dace383d6476ce172
Yizack Rangel <yizackr@gmail.com>
2026-09-18 03:08:33 +0200

    chore(config): centralize Twitch extension ID

--- shared/utils/site.ts
+++ shared/utils/site.ts
@@ -1,9 +1,11 @@
+const TWITCH_EXTENSION_ID = "iczpztdlcdn2gck79hs41x3dzo27g0";
+
 export const SITE = {
   name: "Donobits",
   domain: "donobits.yizack.com",
   twitch: {
     extension: {
-      host: "https://iczpztdlcdn2gck79hs41x3dzo27g0.ext-twitch.tv",
+      host: `https://${TWITCH_EXTENSION_ID}.ext-twitch.tv`,
       helper: "https://extension-files.twitch.tv/helper/v1/twitch-ext.min.js"
     }
   },
