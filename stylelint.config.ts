dfc2d7b77a6a8607c14fcc78a6e578bac4d0a981
Yizack Rangel <yizackr@gmail.com>
2026-09-18 02:04:49 +0200

    chore(stylelint): migrate config to TypeScript

---
+++ stylelint.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from "stylelint";
+
+export default {
+  extends: "stylelint-config-standard-scss",
+  rules: {
+    "length-zero-no-unit": true,
+    "rule-empty-line-before": ["always-multi-line", { except: ["first-nested"] }],
+    "color-function-notation": ["modern", { ignore: ["with-var-inside"] }],
+    "scss/double-slash-comment-empty-line-before": "never"
+  },
+  ignoreFiles: [
+    "node_modules/**/*",
+    ".nuxt/**/*",
+    "dist/**/*",
+    ".output/**/*",
+    "public/**/*"
+  ]
+} satisfies Config;
