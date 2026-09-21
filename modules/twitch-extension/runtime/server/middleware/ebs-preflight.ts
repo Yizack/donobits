b83434d640c071db3006d85aaed678ad2e17e4bc
Yizack Rangel <yizackr@gmail.com>
2026-09-21 18:38:25 +0200

    fix(twitch-extension): preflight does not need OPTIONS method

--- modules/twitch-extension/runtime/server/middleware/ebs-preflight.ts
+++ modules/twitch-extension/runtime/server/middleware/ebs-preflight.ts
@@ -4,7 +4,7 @@ export default defineEventHandler((event) => {
   if (event.method !== "OPTIONS" || !event.path.startsWith("/api/ebs/")) return;
 
   setHeaders(event, {
-    "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+    "Access-Control-Allow-Methods": "GET, POST",
     "Access-Control-Allow-Headers": "Content-Type, Authorization, Channel-Id"
   });
 
