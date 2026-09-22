d4246c6df5e3b1bc28c10b609fa407d8cccc0028
Yizack Rangel <yizackr@gmail.com>
2026-09-22 02:15:17 +0200

    fix(extension): separate source configuration card

--- app/pages/extension/config.vue
+++ app/pages/extension/config.vue
@@ -55,74 +55,78 @@ onMounted(async () => {
 <template>
   <div class="p-1">
     <span v-if="isLoading">Loading...</span>
-    <UCard v-else>
-      <template #header>
-        <img src="~/assets/images/donoclip-logo.svg" alt="Donoclip Logo">
-      </template>
-      <div v-if="broadcaster && extAuth" class="space-y-2">
-        <div v-if="data && !showDonoclipInstructions" class="space-y-2">
-          <UAlert
-            color="neutral"
-            variant="subtle"
-            :description="`Imported ${data.length} items`"
-            :actions="[
-              {
-                label: 'Import Again',
-                onClick: () => {
-                  showDonoclipInstructions = true;
+    <div v-else-if="broadcaster && extAuth" class="space-y-2">
+      <UCard>
+        <template #header>
+          <img src="~/assets/images/donoclip-logo.svg" alt="Donoclip Logo">
+        </template>
+        <div class="space-y-2">
+          <div v-if="data && !showDonoclipInstructions" class="space-y-2">
+            <UAlert
+              color="neutral"
+              variant="subtle"
+              :description="`Imported ${data.length} items`"
+              :actions="[
+                {
+                  label: 'Import Again',
+                  onClick: () => {
+                    showDonoclipInstructions = true;
+                  },
                 },
-              },
-            ]"
-          />
-          <p>Copy and paste the URL below into an OBS Browser Source:</p>
-          <ConfigCopySource :user="broadcaster.name" />
-        </div>
-        <UAlert
-          v-if="error"
-          color="error"
-          variant="subtle"
-          :description="error"
-          close
-          @update:open="(open) => { if (!open) error = '' }"
-        />
-        <form v-if="!data || showDonoclipInstructions" class="space-y-2">
-          <p>Import content from donoclip.com</p>
-          <div>
-            <ol class="list-decimal list-inside space-y-2">
-              <li>
-                Drag this
-                <UButton
-                  label="Import Donoclip"
-                  size="sm"
-                  variant="outline"
-                  :to="donoclip.bookmarklet"
-                  draggable="true"
-                  color="neutral"
-                  :ui="{
-                    base: 'ring-0 outline-2 outline-dashed',
-                  }"
-                />
-                link to your bookmarks bar.
-                <div class="text-xs text-muted mt-2 flex items-center gap-2">
-                  <UBadge label="TIP" size="sm" />
-                  <span>Show bookmarks bar with</span>
-                  <span>
-                    <UKbd value="meta" size="sm" />+<UKbd value="shift" size="sm" />+<UKbd value="b" size="sm" />
-                  </span>
-                </div>
-              </li>
-              <li>
-                Click
-                <UButton size="sm" variant="subtle" label="Donoclip Inbox" @click="donoclip.open()" />
-                to access your Donoclip inbox.
-              </li>
-              <li>Click the "Import Donoclip" bookmark in the Donoclip tab to import your content.</li>
-              <li>Done! Delete the bookmark if you no longer need it.</li>
-            </ol>
+              ]"
+            />
           </div>
-          <UButton v-if="data" color="error" label="Cancel" size="lg" block @click="showDonoclipInstructions = false" />
-        </form>
-      </div>
-    </UCard>
+          <UAlert
+            v-if="error"
+            color="error"
+            variant="subtle"
+            :description="error"
+            close
+            @update:open="(open) => { if (!open) error = '' }"
+          />
+          <form v-if="!data || showDonoclipInstructions" class="space-y-2">
+            <p>Import content from donoclip.com</p>
+            <div>
+              <ol class="list-decimal list-inside space-y-2">
+                <li>
+                  Drag this
+                  <UButton
+                    label="Import Donoclip"
+                    size="sm"
+                    variant="outline"
+                    :to="donoclip.bookmarklet"
+                    draggable="true"
+                    color="neutral"
+                    :ui="{
+                      base: 'ring-0 outline-2 outline-dashed',
+                    }"
+                  />
+                  link to your bookmarks bar.
+                  <div class="text-xs text-muted mt-2 flex items-center gap-2">
+                    <UBadge label="TIP" size="sm" />
+                    <span>Show bookmarks bar with</span>
+                    <span>
+                      <UKbd value="meta" size="sm" />+<UKbd value="shift" size="sm" />+<UKbd value="b" size="sm" />
+                    </span>
+                  </div>
+                </li>
+                <li>
+                  Click
+                  <UButton size="sm" variant="subtle" label="Donoclip Inbox" @click="donoclip.open()" />
+                  to access your Donoclip inbox.
+                </li>
+                <li>Click the "Import Donoclip" bookmark in the Donoclip tab to import your content.</li>
+                <li>Done! Delete the bookmark if you no longer need it.</li>
+              </ol>
+            </div>
+            <UButton v-if="data" color="error" label="Cancel" size="lg" block @click="showDonoclipInstructions = false" />
+          </form>
+        </div>
+      </UCard>
+      <UCard :ui="{ body: 'space-y-2' }">
+        <p>Copy and paste the URL below into an OBS Browser Source:</p>
+        <ConfigCopySource :user="broadcaster.name" />
+      </UCard>
+    </div>
   </div>
 </template>
